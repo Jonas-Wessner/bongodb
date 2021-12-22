f6a2382c164147fea5d4dfedc79bb9e65ee3e1c5
Jonas Weßner <jonas.wessner@stud.h-da.de>
2021-12-22 11:08:00 +0100

    #3 very basic HTTP-server implementation

--- bongo-server/src/main.rs
+++ bongo-server/src/main.rs
@@ -1,3 +1,29 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: do not use unwrap here
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        // TODO: do not use unwrap here
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!(
+        "Request: {}",
+        String::from_utf8_lossy(&buffer[..])
+    );
+
+    // bufwriter only useful when having multiple write calls
+    let mut writer = BufWriter::new(stream);
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    writer.write(response.as_bytes()).unwrap();
+    writer.flush().unwrap();
 }
