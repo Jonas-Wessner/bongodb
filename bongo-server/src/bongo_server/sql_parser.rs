bd2bfcfb4aea82d4d20a64e363d3d83e01b834ca
Jonas Weßner <jonas.wessner@stud.h-da.de>
2021-12-30 22:41:02 +0100

    #4 fix compile time error

--- bongo-server/src/bongo_server/sql_parser.rs
+++ bongo-server/src/bongo_server/sql_parser.rs
@@ -9,12 +9,12 @@ impl SqlParser {
     pub fn parse(sql: &str) -> Result<Statement, ParserError> {
         let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
 
-        let ast = Parser::parse_sql(&dialect, sql)?[0];
+        let ast = &Parser::parse_sql(&dialect, sql)?[0];
 
-        return Self::ast_to_statement(ast);
+        return Self::ast_to_statement(&ast);
     }
 
-    fn ast_to_statement(ast: Ast) -> Result<Statement, ParserError> {
+    fn ast_to_statement(ast: &Ast) -> Result<Statement, ParserError> {
         match ast {
             Ast::Query(_) => {}
             Ast::Insert { .. } => {}
