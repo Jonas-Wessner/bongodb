af85b2789116f0cf39d91b8caa0677287d2b53cd
Jonas Weßner <jonas.wessner@stud.h-da.de>
2022-01-02 14:59:15 +0100

    #4 implement extraction of INSERT

--- bongo-server/src/sql_parser.rs
+++ bongo-server/src/sql_parser.rs
@@ -2,15 +2,27 @@ pub struct SqlParser {}
 
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::{Parser, ParserError};
-use sqlparser::ast::{Statement as Ast, Query, SetExpr, SelectItem, Expr, Select, TableFactor, TableWithJoins, BinaryOperator, OrderByExpr};
+use sqlparser::ast::{Statement as Ast, Query, SetExpr, SelectItem, Expr, Select, TableFactor, TableWithJoins, BinaryOperator, OrderByExpr, ObjectName, Ident};
 use crate::statement::{Statement, SelectItem as BongoSelectItem, Order, Expr as BongoExpr, BinOp as BongoBinOp};
 use std::fmt;
 use std::ascii::escape_default;
 use std::convert::TryFrom;
+use crate::types::{Row, BongoDataType};
+use crate::statement::Expr::Value;
+
+fn syntax_error<T>(err_message: &str) -> Result<T, String> {
+    Err(String::from("Syntax Error: ") + err_message)
+}
+
+fn internal_error<T>(err_message: &str) -> Result<T, String> {
+    Err(String::from("Internal Error: ") + err_message)
+}
+
 
 fn unsupported_feature_err<T>(err_message: &str) -> Result<T, String> {
     Err(String::from("Unsupported Feature: ") + err_message)
 }
+
 
 fn only_single_table_from_err<T>() -> Result<T, String> {
     unsupported_feature_err("Only single identifiers are supported in the FROM clause. Example: Select col_1 FROM table_1")
@@ -18,6 +30,10 @@ fn only_single_table_from_err<T>() -> Result<T, String> {
 
 fn order_by_only_one_column_err<T>() -> Result<T, String> {
     unsupported_feature_err("ORDER BY is only supported with exactly one argument which must be a column name.")
+}
+
+fn insert_list_only_literals<T>() -> Result<T, String> {
+    syntax_error("Only literals can appear in VALUES lists of insert statements")
 }
 
 // TODO: return appropriate errors on all unsafe accesses such as absolute vector indexers
@@ -46,7 +62,7 @@ impl SqlParser {
     fn ast_to_statement(ast: Ast) -> Result<Statement, String> {
         return match ast {
             Ast::Query(query) => { Self::query_to_statement(*query) }
-            Ast::Insert { .. } => { Err(String::from("not yet implemented")) }
+            Ast::Insert { .. } => { Self::insert_to_statement(ast) }
             Ast::Update { .. } => { Err(String::from("not yet implemented")) }
             Ast::Delete { .. } => { Err(String::from("not yet implemented")) }
             Ast::CreateTable { .. } => { Err(String::from("not yet implemented")) }
@@ -60,10 +76,10 @@ impl SqlParser {
             SetExpr::Select(select) => {
                 Ok(
                     Statement::Select {
-                        cols: Self::extract_select_cols(&select)?,
-                        table: Self::extract_select_table(&select)?,
-                        order: Self::extract_select_order(&query.order_by)?,
-                        condition: Self::extract_select_condition(*select)?,
+                        cols: Self::select_extract_cols(&select)?,
+                        table: Self::select_extract_table(&select)?,
+                        order: Self::select_extract_order(&query.order_by)?,
+                        condition: Self::select_extract_condition(*select)?,
                     })
             }
             _ => {
@@ -72,7 +88,7 @@ impl SqlParser {
         };
     }
 
-    fn extract_select_cols(select: &Select) -> Result<Vec<BongoSelectItem>, String> {
+    fn select_extract_cols(select: &Select) -> Result<Vec<BongoSelectItem>, String> {
         let items: Vec<Result<BongoSelectItem, String>> = select.projection.iter()
             .map(|item: &SelectItem| -> Result<BongoSelectItem, String> {
                 return match item {
@@ -118,7 +134,7 @@ impl SqlParser {
         };
     }
 
-    fn extract_select_table(select: &Select) -> Result<String, String> {
+    fn select_extract_table(select: &Select) -> Result<String, String> {
         let tables_with_joins: &Vec<TableWithJoins> = &select.from;
 
         if tables_with_joins.len() != 1 {
@@ -140,7 +156,7 @@ impl SqlParser {
         };
     }
 
-    fn extract_select_condition(select: Select) -> Result<Option<BongoExpr>, String> {
+    fn select_extract_condition(select: Select) -> Result<Option<BongoExpr>, String> {
         return match select.selection {
             Some(cond) => {
                 match BongoExpr::try_from(cond) {
@@ -154,7 +170,7 @@ impl SqlParser {
         };
     }
 
-    fn extract_select_order(order_by_exprs: &Vec<OrderByExpr>) -> Result<Option<Order>, String> {
+    fn select_extract_order(order_by_exprs: &Vec<OrderByExpr>) -> Result<Option<Order>, String> {
         if order_by_exprs.len() == 0 {
             return Ok(None);
         }
@@ -190,11 +206,69 @@ impl SqlParser {
         };
     }
 
-    fn expr_is_supported_order_by(expr: &Expr) -> bool {
-        // so far only ordering by one column is supported -> only identifiers are valid
-        return match expr {
-            Expr::Identifier(_) => { true }
-            _ => { false }
+    fn insert_to_statement(insert: Ast) -> Result<Statement, String> {
+        return match insert {
+            Ast::Insert { table_name, columns, source, .. } => {
+                Ok(
+                    Statement::Insert {
+                        table: Self::insert_extract_table(table_name)?,
+                        cols: Self::insert_extract_cols(columns)?,
+                        rows: Self::insert_extract_rows(*source)?,
+                    })
+            }
+            _ => { internal_error("insert_to_statement should only be called with the Insert variant.") }
+        };
+    }
+
+    fn insert_extract_table(obj_name: ObjectName) -> Result<String, String> {
+        if obj_name.0.len() == 0 {
+            return Err("Specifying more than one table on an INSERT statement is not valid.".to_string());
+        }
+        if obj_name.0.len() > 1 {
+            return Err("Specifying more than one table on an INSERT statement is not valid.".to_string());
+        }
+
+        return Ok(String::from(&obj_name.0[0].value));
+    }
+
+    fn insert_extract_cols(idents: Vec<Ident>) -> Result<Vec<String>, String> {
+        if idents.is_empty() {
+            return unsupported_feature_err("BongoDB does only support INSERT statements \
+            with explicit column lists. Example: INSERT INTO table_1 (col_1, col_2) ..");
+        }
+        Ok(
+            idents.iter().map(|ident| {
+                String::from(&ident.value)
+            }).collect()
+        )
+    }
+
+    fn insert_extract_rows(query: Query) -> Result<Vec<Row>, String> {
+        return match query.body {
+            SetExpr::Values(values) => {
+                values.0.into_iter()
+                    .map(|exprs: Vec<Expr>| {
+                        exprs.into_iter()
+                            .map(|expr: Expr| {
+                                return
+                                    match BongoExpr::try_from(expr) {
+                                        Ok(bongo_expr) => {
+                                            match bongo_expr {
+                                                BongoExpr::Value(data) => { Ok(data) }
+                                                _ => { insert_list_only_literals() }
+                                            }
+                                        }
+                                        Err(_) => { insert_list_only_literals() }
+                                    };
+                            }).collect()
+                    }).collect()
+            }
+            _ => {
+                unsupported_feature_err("In INSERT statements only value lists \
+            are supported by BongoDB. Example: INSERT INTO table_1 (col_1, col_2) VALUES \
+            (1, 'a'),\
+            (2, 'b');")
+            }
         };
     }
 }
@@ -207,7 +281,7 @@ mod tests {
         use sqlparser::tokenizer::Token::Number;
         use sqlparser::ast::Value as ValueEnum;
 
-        use super::super::SqlParser;
+        use crate::sql_parser::SqlParser;
         use crate::statement::{Statement, SelectItem, Order, Expr as BongoExpr, BinOp as BongoBinOp};
         use crate::statement::SelectItem::Wildcard;
         use crate::types::BongoDataType;
@@ -260,6 +334,34 @@ mod tests {
                 table: String::from("table_1"),
                 condition: None,
                 order: None,
+            };
+
+            assert_eq!(statement, Ok(expected_statement));
+        }
+    }
+
+    mod insert {
+        use crate::sql_parser::SqlParser;
+        use crate::statement::Statement;
+        use crate::types::BongoDataType;
+
+        #[test]
+        fn insert_multiple() {
+            let sql = r#"INSERT INTO table_1 (col_1, col_2, col_3) VALUES
+                              (1, 'a', true),
+                              (2, 'b', false),
+                              (3, 'c', Null);"#;
+
+            let statement = SqlParser::parse(sql);
+
+            let expected_statement = Statement::Insert {
+                table: "table_1".to_string(),
+                cols: vec!["col_1".to_string(), "col_2".to_string(), "col_3".to_string()],
+                rows: vec![
+                    vec![BongoDataType::Int(1), BongoDataType::Varchar("a".to_string(), "a".len()), BongoDataType::Bool(true)],
+                    vec![BongoDataType::Int(2), BongoDataType::Varchar("b".to_string(), "b".len()), BongoDataType::Bool(false)],
+                    vec![BongoDataType::Int(3), BongoDataType::Varchar("c".to_string(), "a".len()), BongoDataType::Null]
+                ],
             };
 
             assert_eq!(statement, Ok(expected_statement));
