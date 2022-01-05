2ce1a8f9b02d8ed1028fdeb4a40cf1276d8932d1
Jonas Weßner <jonas.wessner@stud.h-da.de>
2022-01-05 16:28:01 +0100

    #6 add unit tests for BongoRequestParser

--- bongo-server/src/bongo_request.rs
+++ bongo-server/src/bongo_request.rs
@@ -7,6 +7,8 @@ use crate::webserver::RequestParser;
 /// A Request from a bongo client to a `BongoServer`
 ///
 #[derive(Serialize, Deserialize)]
+#[derive(PartialEq)]
+#[derive(Debug)]
 pub struct BongoRequest {
     pub sql: String,
 }
@@ -25,8 +27,6 @@ impl BongoRequestParser {
     }
 }
 
-// TODO: unit test
-
 impl RequestParser<BongoRequest> for BongoRequestParser {
     fn parse(&self, bytes: &[u8]) -> Option<BongoRequest> {
         // unwrapping is safe assuming all bytes are ASCII values
@@ -36,5 +36,50 @@ impl RequestParser<BongoRequest> for BongoRequestParser {
         } else {
             None
         };
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use crate::bongo_request::{BongoRequestParser, BongoRequest};
+    use crate::webserver::RequestParser;
+
+    #[test]
+    fn parse_simple_request() {
+        let request = r#"{ "sql": "SELECT * FROM table_1;" }"#;
+
+        let result = BongoRequestParser::new().parse(request.as_bytes());
+
+        let expected = Some(BongoRequest{
+            sql: "SELECT * FROM table_1;".to_string()
+        });
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn none_on_invalid_request() {
+        // NOTE: one '"' to much
+        let request = r#"{ "sql": ""SELECT * FROM table_1;" }"#;
+
+        let result = BongoRequestParser::new().parse(request.as_bytes());
+
+        let expected = None;
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn escape_quotes_and_curly_braces() {
+        // NOTE: one '"' to much
+        let request = r#"{ "sql": "SELECT * FROM \"table_1\";" }"#;
+
+        let result = BongoRequestParser::new().parse(request.as_bytes());
+
+        let expected = Some(BongoRequest{
+            sql: "SELECT * FROM \"table_1\";".to_string()
+        });
+
+        assert_eq!(expected, result);
     }
 }
